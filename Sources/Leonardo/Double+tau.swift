69d3270d5c8f2192cc481df57b3207f5c4c15b6f
podratz <nick@podratz.de>
2024-08-20 23:53:07 +0200

    Add tau constant

---
+++ Sources/Leonardo/Double+tau.swift
@@ -0,0 +1,13 @@
+//
+//  Double+tau.swift
+//  
+//
+//  Created by Nick on 20.08.24.
+//
+
+import Foundation
+
+extension Double {
+    /// The whole circumference of a circle in radians.
+    static var tau: Double { 2 * .pi }
+}
