c45150c073daddffc8a711cfe8a787c84c53add8
podratz <nick@podratz.de>
2024-08-24 18:34:34 +0200

    Fix plastic ratio implementation

--- Sources/Leonardo/Ratios/plastic.swift
+++ Sources/Leonardo/Ratios/plastic.swift
@@ -5,17 +5,21 @@
 //  Created by Nick on 24.08.24.
 //
 
-import Foundation
+import Numerics
 
 public extension Ratio where ValueType: FloatingPoint {
 
-    static var plastic: Self {
-        var x: ValueType = 1.0
-        for _ in 0..<1000 {
-            let f = x * x * x - x - 1
+    nonisolated static var plastic: Self {
+        var x: ValueType = 1
+        var f: ValueType
+        var i = 0
+        
+        repeat {
+            i += 1
+            f = x * x * x - x - 1
             x -= f / (3 * x * x - 1)
-            if abs(f) < 1e-12 { break }
-        }
+        } while(abs(f) > .ulpOfOne && i < 10)
+        
         return .init(x)
     }
 
