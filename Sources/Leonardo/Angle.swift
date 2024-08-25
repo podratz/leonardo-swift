028f270fb110171e5025280a007f4d41786bbef2
podratz <nick@podratz.de>
2024-08-25 18:59:56 +0200

    Make Angle more generic

--- Sources/Leonardo/Angle.swift
+++ Sources/Leonardo/Angle.swift
@@ -7,17 +7,25 @@
 
 import Numerics
 
-public struct Angle<Radians: Real & ExpressibleByFloatLiteral & ExpressibleByIntegerLiteral>: ExpressibleByFloatLiteral, ExpressibleByIntegerLiteral {
+public struct Angle<Radians: Strideable> {
     
     public let radians: Radians
     
     public init(_ radians: Radians) {
         self.radians = radians
     }
+        
+}
+
+extension Angle: ExpressibleByFloatLiteral where Radians: ExpressibleByFloatLiteral {
     
     public init(floatLiteral radians: Radians.FloatLiteralType) {
         self.radians = Radians(floatLiteral: radians)
     }
+
+}
+
+extension Angle: ExpressibleByIntegerLiteral where Radians: ExpressibleByIntegerLiteral {
     
     public init(integerLiteral radians: Radians.IntegerLiteralType) {
         self.radians = Radians(integerLiteral: radians)
@@ -25,7 +33,7 @@ public struct Angle<Radians: Real & ExpressibleByFloatLiteral & ExpressibleByInt
     
 }
 
-public extension Angle {
+public extension Angle where Radians: FloatingPoint {
     
     var degrees: Radians {
         radians * (360 / .tau)
@@ -34,10 +42,10 @@ public extension Angle {
     init(degrees: Radians) {
         self.radians = degrees * (.tau / 360)
     }
-    
+
 }
 
-public extension Angle {
+public extension Angle where Radians: Real {
     
     typealias ComponentType = Radians
     var complex: Complex<ComponentType> {
