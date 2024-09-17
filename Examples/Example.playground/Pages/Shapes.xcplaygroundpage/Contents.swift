32786384413a63d231755b44dd8c60cc8bfa0038
podratz <nick@podratz.de>
2024-09-17 23:38:59 +0200

    Update squaring the circle playground example

--- Examples/Example.playground/Pages/Shapes.xcplaygroundpage/Contents.swift
+++ Examples/Example.playground/Pages/Shapes.xcplaygroundpage/Contents.swift
@@ -2,27 +2,33 @@ import Leonardo
 import SwiftUI
 import PlaygroundSupport
 
+// MARK: - Squaring the circle.
+
+// That is: creating a square and a circle with the same area.
+// In this example, the transformation is continuous and interactive!
+
+let commonArea: CGFloat = 50_000
+
 struct SquaredCircleCanvas: View {
+
     @State var roundness = 0.5
 
     var body: some View {
-        EmptyView()
-        VStack {
-            ZStack {
-                Circle(area: 50_000)
-                    .opacity(0.2)
-                    .foregroundColor(.gray)
-                SquaredCircle(area: 50_000, roundness: roundness)
-                    .opacity(0.2)
-                    .foregroundColor(.red)
-                Square(area: 50_000)
-                    .opacity(0.2)
-                    .foregroundColor(.gray)
-            }
-            Slider(value: $roundness, in: 0...1)
+        ZStack {
+            Circle(area: commonArea)
+                .opacity(0.2)
+                .foregroundColor(.gray)
+            SquaredCircle(area: commonArea, roundness: roundness)
+                .opacity(0.4)
+                .foregroundColor(.indigo)
+            Square(area: commonArea)
+                .opacity(0.2)
+                .foregroundColor(.gray)
         }
+        Slider(value: $roundness, in: 0...1)
     }
+
 }
 
-let canvas = SquaredCircleCanvas()
-PlaygroundPage.current.setLiveView(canvas)
+let liveView = SquaredCircleCanvas().padding()
+PlaygroundPage.current.setLiveView(liveView)
