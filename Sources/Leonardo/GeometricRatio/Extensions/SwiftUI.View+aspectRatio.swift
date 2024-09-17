31624d460ce692129c77c8b49f1048f31c36fa60
podratz <nick@podratz.de>
2024-09-18 00:54:43 +0200

    Apply generics to aspectRatio() overload

--- Sources/Leonardo/GeometricRatio/Extensions/SwiftUI.View+aspectRatio.swift
+++ Sources/Leonardo/GeometricRatio/Extensions/SwiftUI.View+aspectRatio.swift
@@ -8,24 +8,11 @@
 import SwiftUI
 
 public extension View {
-    
+
+    @inlinable
     nonisolated func aspectRatio(
-        _ aspectRatio: GeometricRatio<Float>,
-        contentMode: ContentMode
-    ) -> some View {
-        self.aspectRatio(CGFloat(aspectRatio.value), contentMode: contentMode)
-    }
-    
-    nonisolated func aspectRatio(
-        _ aspectRatio: GeometricRatio<Double>,
-        contentMode: ContentMode
-    ) -> some View {
-        self.aspectRatio(CGFloat(aspectRatio.value), contentMode: contentMode)
-    }
-    
-    nonisolated func aspectRatio(
-        _ aspectRatio: GeometricRatio<CGFloat>,
-        contentMode: ContentMode
+        _ aspectRatio: GeometricRatio<some BinaryFloatingPoint>,
+        contentMode: ContentMode = .fit
     ) -> some View {
         self.aspectRatio(CGFloat(aspectRatio.value), contentMode: contentMode)
     }
