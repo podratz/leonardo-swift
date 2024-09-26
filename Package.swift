d8ed63bda1d41bde758f6964d615b277a99b7c17
podratz <nick@podratz.de>
2024-09-27 00:24:11 +0200

    Upgrade Mac dependency to v10.15

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -7,20 +6,17 @@ let package = Package(
     name: "Leonardo",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_13)
+        .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Leonardo",
             targets: ["Leonardo"]),
-    ], 
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Leonardo", 
             dependencies: [
