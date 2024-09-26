48b14062f5f11ef973e5e096cb23f042df7916ac
podratz <nick@podratz.de>
2024-09-26 23:42:24 +0200

    Rename test cases

--- Tests/GeometricRatio/ScalingTests.swift
+++ Tests/GeometricRatio/ScalingTests.swift
@@ -5,24 +5,24 @@ import CoreGraphics
 extension GeometricRatioTests {
     struct Scaling {
 
-        @Test("Ratio upscaled() works")
-        func upscaled() {
+        @Test("scale up")
+        func testScaledUp() {
             #expect(10.scaledUp(by: .golden).isApproximatelyEqual(to: 16.18, absoluteTolerance: 0.001))
         }
 
-        @Test("Ratio downscaled() works")
-        func downscaled() {
+        @Test("scale down")
+        func testScaledDown() {
             #expect(10.scaledDown(by: .golden).isApproximatelyEqual(to: 6.18, absoluteTolerance: 0.001))
         }
 
-        @Test("Ratio upscaled() work for CGFloat")
-        func cgFloatUpscaled() {
+        @Test("scale up (CGFloat)")
+        func testScaledUpCGFloat() {
             let cgRatio = GeometricRatio<CGFloat>.golden
             #expect(CGFloat(10).scaledUp(by: cgRatio).isApproximatelyEqual(to: 16.18, absoluteTolerance: 0.001))
         }
 
-        @Test("Ratio downscaled() work for CGFloat")
-        func cgFloatDownscaled() {
+        @Test("scale down (CGFloat)")
+        func testScaledDownCGFloat() {
             let cgRatio = GeometricRatio<CGFloat>.golden
             #expect(CGFloat(10).scaledDown(by: cgRatio).isApproximatelyEqual(to: 6.18, absoluteTolerance: 0.001))
         }
