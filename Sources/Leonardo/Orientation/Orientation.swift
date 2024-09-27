88e80cdd5bf23a7dae5313ac83ba0e7c9fd2cbe2
podratz <nick@podratz.de>
2024-09-27 02:03:26 +0200

    Add unit test for Orientation's Init(rawValue:)

--- Sources/Leonardo/Orientation/Orientation.swift
+++ Sources/Leonardo/Orientation/Orientation.swift
@@ -5,20 +5,22 @@
 //  Created by Nick on 27.09.24.
 //
 
-public enum Orientation: CaseIterable, CustomStringConvertible {
-    case landscape, portrait
+public enum Orientation: Int, CaseIterable, CustomStringConvertible {
+
+    case portrait = -1
+    case landscape = 1
 
     public var description: String {
         switch self {
-        case .landscape: "Landscape"
         case .portrait: "Portrait"
+        case .landscape: "Landscape"
         }
     }
 
     public var sign: Int {
         switch self {
-        case .landscape: 1
         case .portrait: -1
+        case .landscape: 1
         }
     }
 
--- Tests/GeometricRatio/OrientationTests.swift
+++ Tests/GeometricRatio/OrientationTests.swift
@@ -5,6 +5,11 @@ import Testing
 extension GeometricRatioTests {
     struct Orientations {
 
+        @Test("Init(rawValue:)", arguments: [(.portrait, -1), (.landscape, 1)] as [(Orientation, Int)])
+        func testInitFromRawValue(orientation: Orientation, rawValue: Int) {
+            #expect(orientation == Orientation(rawValue: rawValue))
+        }
+
         @Test("allCases", arguments: [.landscape, .portrait] as [Orientation])
         func testAllCasesContains(orientation: Orientation) {
             #expect(Orientation.allCases.contains(orientation) )
