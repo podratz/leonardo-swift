95bfd96f8d580d00a6b5a985b2de855347f8c6d9
Nick Podratz <nick@podratz.de>
2024-11-08 17:35:46 +0100

    Remove unnecessary type conversions

--- Sources/Leonardo/GeometricRatio/GeometricRatio+Quotient.swift
+++ Sources/Leonardo/GeometricRatio/GeometricRatio+Quotient.swift
@@ -16,12 +16,12 @@ public extension GeometricRatio where Value: FloatingPoint {
 
     /// The reciprocal of the ratio. The invariant (1 / quotient == reciprocal) holds.
     var reciprocal: Value {
-        Value(1) / quotient
+        1 / quotient
     }
 
     init(_ quotient: Value, name: String? = nil) {
         self.numerator = quotient
-        self.denominator = Value(1)
+        self.denominator = 1
         self.name = name
     }
 
