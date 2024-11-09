7ad8a3c26ca3f8978b64a5258bffa49470198be4
Nick Podratz <nick@podratz.de>
2024-11-09 15:05:24 +0100

    Refactor CustomStringConvertible

--- Sources/Leonardo/GeometricRatio/Conformances/+CustomStringConvertible.swift
+++ Sources/Leonardo/GeometricRatio/Conformances/+CustomStringConvertible.swift
@@ -13,31 +13,42 @@ extension GeometricRatio: CustomStringConvertible where Value == FloatLiteralTyp
         "\(numericDescription) (\(qualifiedName))"
     }
 
-    public var numericDescription: String {
-        let numerator = NSNumber(floatLiteral: numerator)
-        let formattedNumerator = Self.termFormatter.string(from: numerator)!
-
-        let denominator = NSNumber(floatLiteral: denominator)
-        let formattedDenominator = Self.termFormatter.string(from: denominator)!
-        
-        return formattedNumerator + Self.dividerSymbol + formattedDenominator
-    }
-
     public var qualifiedName: String {
         let namePrefix = if let name { name } else { "Unnamed" }
         return namePrefix + " Ratio"
     }
+    
+    public var numericDescription: String {
+        let formattedNumerator = numerator.termFormatted
+        let formattedDenominator = denominator.termFormatted
         
+        return formattedNumerator + Self.dividerSymbol + formattedDenominator
+    }
+        
+    private static var dividerSymbol: String {
+        UnicodeScalar(0x2236).flatMap(String.init) ?? ":"
+    }
+
+}
+
+fileprivate extension FloatLiteralType {
+    
+    var termFormatted: String {
+        formatTerm(self)
+    }
+    
+    private func formatTerm(_ term: Self) -> String {
+        let numerator = NSNumber(floatLiteral: term)
+        let formattedNumerator = Self.termFormatter.string(from: numerator)!
+        return formattedNumerator
+    }
+
     private static var termFormatter: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
         formatter.maximumFractionDigits = 3
         formatter.locale = .init(identifier: "en_US")
         return formatter
-    }
-    
-    private static var dividerSymbol: String {
-        UnicodeScalar(0x2236).flatMap(String.init) ?? ":"
     }
 
 }
