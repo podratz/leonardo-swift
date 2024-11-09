86799f1da049ae6aabc1d69fc26e2d3184cbf823
Nick Podratz <nick@podratz.de>
2024-11-09 03:45:00 +0100

    Improve CustomStringConvertible formatting

--- Sources/Leonardo/GeometricRatio/Conformances/+CustomStringConvertible.swift
+++ Sources/Leonardo/GeometricRatio/Conformances/+CustomStringConvertible.swift
@@ -5,13 +5,31 @@
 //  Created by Nick Podratz on 09.11.24.
 //
 
-extension GeometricRatio: CustomStringConvertible {
+import Foundation
+
+extension GeometricRatio: CustomStringConvertible where Value == FloatLiteralType {
     
     public var description: String {
-        let numerator = "\(numerator)"
-        let denominator = "\(denominator)"
-        let name = if let name { name } else { "Unnamed" }
-        return numerator + ":" + denominator + " " + "(" + name + " Ratio" + ")"
+        "\(numericDescription) (\(qualifiedName))"
+    }
+
+    public var numericDescription: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 3
+
+        let numerator = NSNumber(floatLiteral: numerator)
+        let formattedNumerator = formatter.string(from: numerator)!
+
+        let denominator = NSNumber(floatLiteral: denominator)
+        let formattedDenominator = formatter.string(from: denominator)!
+        
+        return formattedNumerator + ":" + formattedDenominator
+    }
+
+    public var qualifiedName: String {
+        let namePrefix = if let name { name } else { "Unnamed" }
+        return namePrefix + " Ratio"
     }
     
 }
