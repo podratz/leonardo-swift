42f2bafadc3b922a5c932b9ae10f874b90214638
Nick Podratz <nick@podratz.de>
2024-11-15 21:48:24 +0100

    Simplify Proportion implementation

--- Sources/Leonardo/Proportion.swift
+++ Sources/Leonardo/Proportion.swift
@@ -6,41 +6,18 @@
 //
 
 public struct Proportion<Value: FloatingPoint> {
-    
-    let ratio: GeometricRatio<Value>
-    let providedValue: ProvidedValue
-    
-    public var numerator: Value {
-        switch providedValue {
-        case .numerator(let numerator):
-            numerator
-        case .denominator(let denominator):
-            ratio.quotient * denominator
-        }
-    }
-    
-    public var denominator: Value {
-        switch providedValue {
-        case .numerator(let numerator):
-            ratio.reciprocal * numerator
-        case .denominator(let denominator):
-            denominator
-        }
-    }
-    
-    public init(_ ratio: GeometricRatio<Value>, numerator value: Value) {
-        self.ratio = ratio
-        self.providedValue = .numerator(value)
+
+    public let numerator: Value
+    public let denominator: Value
+
+    public init(_ ratio: GeometricRatio<Value>, numerator: Value) {
+        self.numerator = numerator
+        self.denominator = ratio.reciprocal * numerator
     }
 
-    public init(_ ratio: GeometricRatio<Value>, denominator value: Value) {
-        self.ratio = ratio
-        self.providedValue = .denominator(value)
+    public init(_ ratio: GeometricRatio<Value>, denominator: Value) {
+        self.numerator = ratio.quotient * denominator
+        self.denominator = denominator
     }
-    
-    enum ProvidedValue {
-        case numerator(_ value: Value)
-        case denominator(_ value: Value)
-    }
-    
+
 }
