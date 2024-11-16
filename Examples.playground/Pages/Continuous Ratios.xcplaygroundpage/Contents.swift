f3180d99b2ed1325cbd397cdbb9295827df9d862
Nick Podratz <nick@podratz.de>
2024-11-16 02:21:45 +0100

    Extend Continuous Ratio playground

--- Examples.playground/Pages/Continuous Ratios.xcplaygroundpage/Contents.swift
+++ Examples.playground/Pages/Continuous Ratios.xcplaygroundpage/Contents.swift
@@ -2,27 +2,55 @@ import Leonardo
 import SwiftUI
 import PlaygroundSupport
 
-let ratio = ContinuedRatio<Double>(.gold, numberOfTerms: 3)
 
-struct SquaredCircleCanvas: View {
+struct Canvas: View {
 
     let width: Double = 300
     
-    var widthSegments: [Double] {
-        width.segmented(by: ratio)
+    var body: some View {
+        
+        // Golden ratio
+        ForEach(1...5, id: \.self) { numberOfTerms in
+            let ratio = ContinuedRatio<Double>(.gold, numberOfTerms: numberOfTerms)
+            let segments = width.segmented(by: ratio)
+            return row(segments, color: .yellow)
+        }
+
+        // Silver ratio
+        ForEach(1...5, id: \.self) { numberOfTerms in
+            let ratio = ContinuedRatio<Double>(.silver, numberOfTerms: numberOfTerms)
+            let segments = width.segmented(by: ratio)
+            return row(segments, color: .gray)
+        }
+
+        // Bronce ratio
+        ForEach(1...5, id: \.self) { numberOfTerms in
+            let ratio = ContinuedRatio<Double>(.bronce, numberOfTerms: numberOfTerms)
+            let segments = width.segmented(by: ratio)
+            return row(segments, color: .orange)
+        }
+
+        // Plastic ratio
+        ForEach(1...5, id: \.self) { numberOfTerms in
+            let ratio = ContinuedRatio<Double>(.plastic, numberOfTerms: numberOfTerms)
+            let segments = width.segmented(by: ratio)
+            return row(segments, color: .green)
+        }
     }
     
-    var body: some View {
-        HStack {
-            ForEach(widthSegments, id: \.self) { widthSegment in
+    func row(_ segments: [Double], color: Color) -> some View {
+        HStack(spacing: 0) {
+            ForEach(segments, id: \.self) { segmentWidth in
                 Rectangle()
-                    .stroke(lineWidth: 3)
-                    .frame(width: widthSegment, height: 50)
+                    .stroke(lineWidth: 2)
+                    .fill(color)
+                    .frame(width: segmentWidth, height: 50)
             }
         }
+
     }
 
 }
 
-let liveView = SquaredCircleCanvas().padding()
+let liveView = Canvas().padding()
 PlaygroundPage.current.setLiveView(liveView)
