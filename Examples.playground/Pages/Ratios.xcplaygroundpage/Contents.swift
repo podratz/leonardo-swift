ded38eb7c54bba0dc4e35272907ddecab33e3996
Nick Podratz <nick@podratz.de>
2024-12-14 20:26:01 +0100

    Add example to playground

--- Examples.playground/Pages/Ratios.xcplaygroundpage/Contents.swift
+++ Examples.playground/Pages/Ratios.xcplaygroundpage/Contents.swift
@@ -7,3 +7,7 @@ let doubling: Ratio = 2
 for neighbor in 16.neighborhood(ratio: doubling, radius: 4) {
     neighbor
 }
+
+let ratio = ContinuedRatio<Double>(.gold[2], numberOfTerms: 3)
+let segments = 100.segmented(by: ratio)
+
